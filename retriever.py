8a11a01c03bcaf39d941e98c4a757a56ee0cd6a7
sandromuse <sandromusela@gmail.com>
2026-04-14 00:26:10 +0400

    fix vectorstore initialization

--- retriever.py
+++ retriever.py
@@ -1,9 +1,9 @@
-from dotenv import load_dotenv #import a fucntion that helps me read the env file to use my key 
-from langchain_community.embeddings import HuggingFaceEmbeddings # imports a functions that embedds each chunk as a number
-from langchain_community.vectorstores import Chroma # a vector database that store the embeddings the chunks 
+from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
-embeddings = HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2") 
-vectorstore =  Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 def retrieve(question):
-        return vectorstore.similarity_search(question,k = 3)
+    vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+    return vectorstore.similarity_search(question, k=3)
