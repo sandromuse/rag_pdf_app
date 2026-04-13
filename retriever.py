8f58d585e0882fe7893d4c6573270fd013c720c7
sandromuse <sandromusela@gmail.com>
2026-04-14 00:30:27 +0400

    switch to in-memory chromadb

--- retriever.py
+++ retriever.py
@@ -1,9 +1,8 @@
-from dotenv import load_dotenv
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 def retrieve(question):
-    vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+    vectorstore = Chroma(embedding_function=embeddings)
     return vectorstore.similarity_search(question, k=3)
